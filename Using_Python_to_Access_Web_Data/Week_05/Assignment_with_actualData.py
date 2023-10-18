61a86f9ecd4143b051d2a8fe72ee0bbec7c4116f
Hansani Gunathilaka <hansinima20003@gmail.com>
2023-10-18 14:08:11 +0530

    Create Assignment_with_actualData.py

---
+++ Using_Python_to_Access_Web_Data/Week_05/Assignment_with_actualData.py
@@ -0,0 +1,16 @@
+import urllib.request, urllib.parse, urllib.error
+import xml.etree.ElementTree as ET
+
+fhand = urllib.request.urlopen('http://py4e-data.dr-chuck.net/comments_1797950.xml')
+
+data = fhand.read().decode()
+
+tree = ET.fromstring(data)
+counts = tree.findall('.//count')
+print('Count: ',len(counts))
+sum = 0
+for item in counts:
+    # print('Count: ',item.text)
+    sum = sum + (int)(item.text)
+
+print(sum)
