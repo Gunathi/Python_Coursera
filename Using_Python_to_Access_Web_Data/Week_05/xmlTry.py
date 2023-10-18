568d1574b2a948fd9f2f184310b434c63a18fccc
Hansani Gunathilaka <hansinima20003@gmail.com>
2023-10-18 13:35:33 +0530

    Create xmlTry.py

---
+++ Using_Python_to_Access_Web_Data/Week_05/xmlTry.py
@@ -0,0 +1,34 @@
+import xml.etree.ElementTree as ET
+
+#First Example
+data = '''<person>
+    <name> Hansani </name>
+    <phone type = "int1"> +94702876596 </phone>
+    <email hide = "yes"/>   
+</person>'''
+
+tree = ET.fromstring(data)
+print("Name: ",tree.find('name').text)
+print("Attr: ",tree.get("hide"))
+
+#Second Example
+input = '''<stuff>
+    <users>
+        <user x = "2">
+            <id>001</id>
+            <name> Chuck </name>
+        </user>
+        <user x = "7">
+            <id>009</id>
+            <name> Hansani </name>
+        </user>
+    </users>
+</stuff>'''
+
+stuff = ET.fromstring(input)
+userList = stuff.findall('users/user')
+print('User count: ',len(userList))
+for item in userList:
+    print('Name: ',item.find('name').text)
+    print('ID: ',item.find('id').text)
+    print('Attribute: ',item.get('x'))
