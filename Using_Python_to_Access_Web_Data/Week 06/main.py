c8010c4af7e6886141cadb0a753a16d595f870b4
Hansani Gunathilaka <hansinima20003@gmail.com>
2023-10-28 16:54:13 +0530

    Add files via upload

---
+++ Using_Python_to_Access_Web_Data/Week 06/main.py
@@ -0,0 +1,39 @@
+import json
+
+#Using dictionary
+data = '''{
+    "name" : "Hansani",
+    "phone" : {
+        "type" : "intl",
+        "number" : "+94702876596"
+    },
+    "email" : {
+        "hide" : "yes"
+    }
+}'''
+
+info = json.loads(data)
+print('Name: ', info["name"])
+print("Hide: ", info["email"]["hide"])
+
+print("")
+
+#Using lists
+input = '''[
+    { "id" : "001",
+      "x" : "2",
+      "name" : "Hansani"
+    },
+    { "id" : "009",
+      "x" : "7",
+      "name" : "Nimasha"
+    }
+]'''
+
+info = json.loads(input)
+print("User count: ", len(info))
+
+for item in info:
+    print("id: ", item["id"])
+    print("attr: ", item["x"])
+    print("Name: ", item["name"])
